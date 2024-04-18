8ca2837f391b42837ba9983409eb9cc3a58a6bb3
Your Name <dannydotdev@gmail.com>
2024-04-18 17:48:43 +0100

    Use context

--- src/Hooks/UseContext.jsx
+++ src/Hooks/UseContext.jsx
@@ -4,7 +4,9 @@ const UseContext = () => {
   return (
     <div>
       <h4>Use Context</h4>
-      {/* There are two things in context  */}
+      {/* There are two things to note in useContext 1. The Context provider, which we want to wrap all the codes that needs access to
+      2. Single props passed, which is value={whatever the value of the context is}
+      */}
     </div>
   );
 };
