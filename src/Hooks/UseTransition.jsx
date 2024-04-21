bccf2edb485c0fea6025f3db7c2c54d6057777c9
Your Name <dannydotdev@gmail.com>
2024-04-21 18:23:07 +0100

    use transition hero

--- src/Hooks/UseTransition.jsx
+++ src/Hooks/UseTransition.jsx
@@ -4,7 +4,12 @@ const UseTransition = () => {
   return (
     <div>
       <h2 className="mb-4 text-2xl font-bold">USE TRANSITION</h2>
-      <p></p>
+      <p>
+        Using transitions in React can add smooth animations to your
+        application, making it more visually appealing and enhancing the user
+        experience. One common library used for animations and transitions in
+        React is react-transition-group.
+      </p>
     </div>
   );
 };
